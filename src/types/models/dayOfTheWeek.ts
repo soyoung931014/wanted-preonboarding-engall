ba249e4b879e78f017e29cb5f36fcaf4c6af3130
park_so_young <soyoung931014@gmail.com>
2022-07-28 19:50:52 +0900

    chore: 타입 지정

---
+++ src/types/models/dayOfTheWeek.ts
@@ -0,0 +1,20 @@
+export interface IWeek {
+  Sun: '0';
+}
+
+export interface info {
+  id: number;
+  day: findWeek;
+  start: 'string';
+  end: 'string';
+}
+
+export interface findWeek {
+  0: 'Sun';
+  1: 'Mon';
+  2: 'Tue';
+  3: 'Wed';
+  4: 'THU';
+  5: 'FRI';
+  6: 'Sat';
+}
