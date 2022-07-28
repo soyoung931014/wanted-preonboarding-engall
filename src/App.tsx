03a03e722fa0facee79af5bf6234a582f91f627d
park_so_young <soyoung931014@gmail.com>
2022-07-28 19:54:26 +0900

    chore:  ThemeProvider, GlobalStyle, Layout 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+
+import Router from '@routes/Router';
+import GlobalStyle from './styles/GlobalStyles';
+import Layout from '@layout/Layout';
+import theme from '@styles/theme';
 
 function App() {
-  return <div>helloworld!</div>;
+  return (
+    <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <Layout>
+          <Router />
+        </Layout>
+      </ThemeProvider>
+    </BrowserRouter>
+  );
 }
 
 export default App;
