457e98dc217c230865097454cfdfd78d099465cc
park_so_young <soyoung931014@gmail.com>
2022-07-29 03:11:01 +0900

    chore: react-query 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
+import { QueryClientProvider, QueryClient } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
 
 import Router from '@routes/Router';
 import GlobalStyle from './styles/GlobalStyles';
@@ -8,15 +10,19 @@ import Layout from '@layout/Layout';
 import theme from '@styles/theme';
 
 function App() {
+  const queryClient = new QueryClient();
   return (
-    <BrowserRouter>
-      <ThemeProvider theme={theme}>
-        <GlobalStyle />
-        <Layout>
-          <Router />
-        </Layout>
-      </ThemeProvider>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <GlobalStyle />
+          <Layout>
+            <Router />
+          </Layout>
+        </ThemeProvider>
+      </BrowserRouter>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 }
 
