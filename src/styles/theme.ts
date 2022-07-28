4e6d815e6f89b0dad4e48552cb81530c6efa9be6
park_so_young <soyoung931014@gmail.com>
2022-07-28 19:52:52 +0900

    chore: 글로벌스타일 및 theme생성

---
+++ src/styles/theme.ts
@@ -0,0 +1,15 @@
+const color = {
+  black: '#272727',
+  buttonColor: '#3074d8',
+  grey_02: '#F7F6FB',
+  grey_03: '#F3F3F3',
+  grey_04: '#CFCFCF',
+  grey_05: '#8F8F8F',
+  grey_06: '#4B4B4B',
+};
+
+const theme = {
+  color,
+};
+
+export default theme;
