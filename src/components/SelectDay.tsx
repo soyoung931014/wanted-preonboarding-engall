b9fcec695da7f9f7b287f1bdcdd3d74535a82875
park_so_young <soyoung931014@gmail.com>
2022-07-29 20:17:55 +0900

    design: css

--- src/components/SelectDay.tsx
+++ src/components/SelectDay.tsx
@@ -5,6 +5,7 @@ export default function SelectDay({ handleDayofWeek }: any) {
   return (
     <Container>
       <Title>Repeat on</Title>
+
       <DayBox onClick={e => handleDayofWeek(e)}>
         <DayButton name="Mon">Monday</DayButton>
         <DayButton name="Tue">Tuesday</DayButton>
@@ -20,18 +21,23 @@ export default function SelectDay({ handleDayofWeek }: any) {
 
 const Container = styled.div`
   display: flex;
+  margin-top: 20px;
 `;
-const DayBox = styled.div`
-  display: flex;
-`;
+const DayBox = styled.div``;
 const Title = styled.div`
   margin-left: 10px;
   width: 100px;
   font-size: 20px;
   font-weight: 600;
+  position: relative;
+  top: 12px;
 `;
+
 const DayButton = styled.button`
-  border: solid black 1px;
+  border: solid 2px #b4b4b4;
   padding: 10px;
-  width: 100px;
+  width: 140px;
+  margin-right: 30px;
+  background-color: white;
+  font-size: 18px;
 `;
